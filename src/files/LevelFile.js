f6663b1566741da14fc5209376a83adfd8b69f79
Sazykin Georgii <sazykin.georgii@gmail.com>
2025-04-30 12:02:25 +0300

    refactor: move constructor after static class variables

--- src/files/LevelFile.js
+++ src/files/LevelFile.js
@@ -5,6 +5,49 @@ import EnumProperty from '../properties/EnumProperty.js';
 
 export default class LevelFile {
 
+    static FOG_DENSITY_HEADER = '0C 00 00 00 46 6F 67 20 44 65 6E 73 69 74 79 00 0E 00 00 00 46 6C 6F 61 74 50 72 6F 70 65 72 74 79 00 00 00 00 00 04 00 00 00 00';
+
+    static MUSIC_HEADER = '0B 00 00 00 4C 65 76 65 6C 4D 75 73 69 63 00 0C 00 00 00 53 74 72 50 72 6F 70 65 72 74 79 00 00 00 00 00';
+
+    static ACTOR_PROPS_HEADER = '41 63 74 6F 72 50 72 6F 70 65 72 74 69 65 73 00 0E 00 00 00 41 72 72 61 79 50 72 6F 70 65 72 74 79 00 01 00 00 00 0D 00 00 00 42 79 74 65 50 72 6F 70 65 72 74 79 00 00 00 00 00';
+
+    static TILES_X_HEADER = '08 00 00 00 54 69 6C 65 73 20 58 00 0C 00 00 00 49 6E 74 50 72 6F 70 65 72 74 79 00 00 00 00 00 04 00 00 00 00';
+
+    static TILES_Y_HEADER = '08 00 00 00 54 69 6C 65 73 20 59 00 0C 00 00 00 49 6E 74 50 72 6F 70 65 72 74 79 00 00 00 00 00 04 00 00 00 00';
+
+    static LEVEL_ENVIRONMENT_OPTIONS = {
+        None: {
+            name: 'None',
+            musicName: 'None',
+            terrainPath: null,
+            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator9',
+        },
+        Rome: {
+            name: 'Rome',
+            musicName: 'Rome',
+            terrainPath: '/Game/MapEditor/Materials/MI_EditorGround_Rome.MI_EditorGround_Rome',
+            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator1',
+        },
+        Medieval: {
+            name: 'Medieval',
+            musicName: 'Medieval',
+            terrainPath: '/Game/MapEditor/Materials/MI_EditorGround_Medieval.MI_EditorGround_Medieval',
+            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator2',
+        },
+        Egypt: {
+            name: 'Egypt',
+            musicName: 'Egypt',
+            terrainPath: '/Game/MapEditor/Materials/MI_EditorGround_Egypt.MI_EditorGround_Egypt',
+            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator0',
+        },
+        Wood: {
+            name: 'Wood',
+            musicName: 'Wood',
+            terrainPath: '/Game/MapEditor/Materials/MI_EditorGround_Wood.MI_EditorGround_Wood',
+            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator4',
+        }
+    };
+
     constructor(arrayBuffer) {
         this.file = new UnrealEngineFile(arrayBuffer);
 
@@ -76,49 +119,6 @@ export default class LevelFile {
             },
         });
     }
-
-    static FOG_DENSITY_HEADER = '0C 00 00 00 46 6F 67 20 44 65 6E 73 69 74 79 00 0E 00 00 00 46 6C 6F 61 74 50 72 6F 70 65 72 74 79 00 00 00 00 00 04 00 00 00 00';
-
-    static MUSIC_HEADER = '0B 00 00 00 4C 65 76 65 6C 4D 75 73 69 63 00 0C 00 00 00 53 74 72 50 72 6F 70 65 72 74 79 00 00 00 00 00';
-
-    static ACTOR_PROPS_HEADER = '41 63 74 6F 72 50 72 6F 70 65 72 74 69 65 73 00 0E 00 00 00 41 72 72 61 79 50 72 6F 70 65 72 74 79 00 01 00 00 00 0D 00 00 00 42 79 74 65 50 72 6F 70 65 72 74 79 00 00 00 00 00';
-
-    static TILES_X_HEADER = '08 00 00 00 54 69 6C 65 73 20 58 00 0C 00 00 00 49 6E 74 50 72 6F 70 65 72 74 79 00 00 00 00 00 04 00 00 00 00';
-
-    static TILES_Y_HEADER = '08 00 00 00 54 69 6C 65 73 20 59 00 0C 00 00 00 49 6E 74 50 72 6F 70 65 72 74 79 00 00 00 00 00 04 00 00 00 00';
-
-    static LEVEL_ENVIRONMENT_OPTIONS = {
-        None: {
-            name: 'None',
-            musicName: 'None',
-            terrainPath: null,
-            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator9',
-        },
-        Rome: {
-            name: 'Rome',
-            musicName: 'Rome',
-            terrainPath: '/Game/MapEditor/Materials/MI_EditorGround_Rome.MI_EditorGround_Rome',
-            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator1',
-        },
-        Medieval: {
-            name: 'Medieval',
-            musicName: 'Medieval',
-            terrainPath: '/Game/MapEditor/Materials/MI_EditorGround_Medieval.MI_EditorGround_Medieval',
-            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator2',
-        },
-        Egypt: {
-            name: 'Egypt',
-            musicName: 'Egypt',
-            terrainPath: '/Game/MapEditor/Materials/MI_EditorGround_Egypt.MI_EditorGround_Egypt',
-            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator0',
-        },
-        Wood: {
-            name: 'Wood',
-            musicName: 'Wood',
-            terrainPath: '/Game/MapEditor/Materials/MI_EditorGround_Wood.MI_EditorGround_Wood',
-            vegetationEnumName: 'E_ModelEnvironment::NewEnumerator4',
-        }
-    };
 
     hasWidth() {
         return this.width.exists();
